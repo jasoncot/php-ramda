951fd3115bc411f5e1b8d6a501f6d908a3f90011
Jason Cotton <jwcotton@sbgtv.com>
2018-09-07 10:38:50 -0700

    fleshing out the reduce function

--- src/reduce.php
+++ src/reduce.php
@@ -1,14 +1,24 @@
 <?php
 namespace Trailoff\PHRamda;
 
-function reduce($fn, $val, $arr) {
-    if (!isset($fn) || !isset($val) || !isset($arr)) {
+function reduce($callback, $val, $arr)
+{
+    if (!isset($callback) || !isset($val) || !isset($arr)) {
         return null;
     }
     $count = count($arr);
     $acc = $val;
     for ($i = 0; $i < $count; $i += 1) {
-        $acc = $fn($acc, $arr[$i]);
+        $acc = $callback($acc, $arr[$i]);
     }
     return $acc;
+}
+
+function c_reduce($callback)
+{
+    return function ($initialValue) use ($callback) {
+        return function ($reducable) use ($callback, $initialValue) {
+            return reduce($callback, $initialValue, $reducable);
+        };
+    };
 }
