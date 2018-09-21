4720c32671372e76f1785ca07eee4783778699c1
Jason Cotton <jwcotton@sbgtv.com>
2018-09-21 10:37:39 -0700

    rename variable to fix linting error.

--- src/isEmpty.php
+++ src/isEmpty.php
@@ -1,18 +1,18 @@
 <?php
 namespace PHRamda;
 
-function isEmpty($a)
+function isEmpty($arg0)
 {
-    if (!isset($a) || $a === null) {
+    if (!isset($arg0) || $arg0 === null) {
         return false;
     }
-    if (is_array($a) && count($a) === 0) {
+    if (is_array($arg0) && count($arg0) === 0) {
         return true;
     }
-    if (is_object($a) && count(array_keys(get_object_vars($a)))) {
+    if (is_object($arg0) && count(array_keys(get_object_vars($arg0)))) {
         return true;
     }
-    if (is_string($a) && $a === "") {
+    if (is_string($arg0) && $arg0 === "") {
         return true;
     }
     return false;
