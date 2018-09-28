a9089e49c4c5ccd22d1e5a3f795816affca83c3b
Jason Cotton <jwcotton@sbgtv.com>
2018-09-28 15:10:23 -0700

    Fix issue preventing usage because string isn't callable.

--- src/pipe.php
+++ src/pipe.php
@@ -3,12 +3,12 @@ namespace PHRamda;
 
 use function PHRamda\reduce;
 
-function pipe(...$callbacks)
+function pipe(...$callbacks): callable
 {
     return function ($arg0) use ($callbacks) {
         return reduce(
-            function ($acc, callable $callback) {
-                return $callback($acc);
+            function ($acc, $callback) {
+                return call_user_func($callback, $acc);
             },
             $arg0,
             $callbacks
