0b42e7450507d9fc5db4399d6f76f4f4167287cb
Jason Cotton <jasoncot@gmail.com>
2019-02-25 23:58:30 -0800

    Added coverage for the comparitor function

---
+++ tests/ComparitorTest.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace PHRamdaTests;
+
+use PHPUnit\Framework\TestCase;
+use function PHRamda\comparitor;
+
+final class ComparitorTest extends TestCase
+{
+    /**
+     * @covers \PHRamda\comparitor
+     */
+    public function testComparitor(): void
+    {
+        $byInt = comparitor(function (int $a, int $b) {
+            return $a < $b ? true : false;
+        });
+
+        self::assertEquals(-1, $byInt(2, 3));
+        self::assertEquals(1, $byInt(3, 2));
+        self::assertEquals(-1, $byInt(3, 3));
+    }
+}
